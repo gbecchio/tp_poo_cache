4903c148f2cb60114930fdd0d34b62007965c63e
gregory <gregory.becchio@ameliste.com>
2015-12-23 11:20:04 +0100

    correction

--- new/lib/OCFram/HTTPRequest.php
+++ new/lib/OCFram/HTTPRequest.php
@@ -1,46 +1,6 @@
 <?php
 namespace OCFram;
-<<<<<<< HEAD
 
-class HTTPRequest
-{
-    protected $method;
-    protected $uri;
-
-    public function cookieData($key)
-    {
-        return ($this->cookieExists($key))?$_COOKIE[$key]:null;
-    }
-    public function cookieExists($key)
-    {
-        return (bool) isset(_COOKIE[$key]);
-    }
-    public function getData($key)
-    {
-        return $this->$_GET[$key];
-    }
-    public function getExists($key)
-    {
-        return (bool) isset($_GET[$key]);
-    }
-    public function method()
-    {
-        return $this->method;
-    }
-    public function postData($key)
-    {
-        return $_POST[$key];
-    }
-    public function postExists($key)
-    {
-        return (bool) isset($_POST[$key]);
-    }
-    public function requestURI()
-    {
-        return $this->uri;
-    }
-=======
- 
 class HTTPRequest extends ApplicationComponent
 {
   public function cookieData($key)
@@ -82,5 +42,4 @@ class HTTPRequest extends ApplicationComponent
   {
     return $_SERVER['REQUEST_URI'];
   }
->>>>>>> 619c8188948b4e2c5d314eed51642a8a3386e453
 }
